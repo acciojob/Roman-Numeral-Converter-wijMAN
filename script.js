aad989381fc3c29e1fe35717f4f9683bd8ca3518
wijman <wish6242@gmail.com>
2023-01-13 21:47:53 +0530

    1st commit

--- script.js
+++ script.js
@@ -10,11 +10,43 @@ function convertToRoman(num) {
     };
 
   //your code here
-
+  let s="";
+  while(num!=0){
+    if(num>=1000){
+      num-=1000;
+      s=s+'M';
+    }
+    else if(num>=500){
+      num-=500;
+      s=s+'D';
+    }
+    else if(num>=100){
+      num-=100;
+      s=s+'C';
+    }
+    else if(num>=50){
+      num-=50;
+      s=s+'L';
+    }
+    else if(num>=10){
+      num-=10;
+      s=s+'X';
+    }
+    else if(num>=5){
+      num-=5;
+      s=s+'V';
+    }
+    else if(num>=1){
+      num-=1;
+      s=s+'I';
+    }
+  }
+  return s;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+// console.log(convertToRoman(0));
+
 
 
 
